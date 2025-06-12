88f8beb8e44f2c7c0985c1972d8e6be544bbab13
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-12 17:21:48 +0600

    create profileLink

---
+++ src/components/Sidebar/ProfileLink.jsx
@@ -0,0 +1,35 @@
+import { Box,Tooltip,Link, Avatar } from '@chakra-ui/react';
+import { AiFillHome } from 'react-icons/ai';
+import {Link as RouterLink} from 'react-router-dom';
+
+const ProfileLink = () => {
+    // const authUser = useAuthStore((state) => state.user);
+  return (
+    <Tooltip
+			hasArrow
+			label={"Profile"}
+			placement='right'
+			ml={1}
+			openDelay={500}
+			display={{ base: "block", md: "none" }}
+		>
+			<Link
+				display={"flex"}
+				to={"#"}
+				as={RouterLink}
+				alignItems={"center"}
+				gap={4}
+				_hover={{ bg: "whiteAlpha.400" }}
+				borderRadius={6}
+				p={2}
+				w={{ base: 10, md: "full" }}
+				justifyContent={{ base: "center", md: "flex-start" }}
+			>
+				<Avatar size={"sm"} src={"/logo.png"} />
+				<Box display={{ base: "none", md: "block" }}>Profile</Box>
+			</Link>
+		</Tooltip>
+  )
+}
+
+export default ProfileLink
