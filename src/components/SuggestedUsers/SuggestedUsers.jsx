a4ab42abf22e815d16f25d3b3cbee870ffceb27f
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-13 15:29:30 +0600

    design suggested users

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,29 @@
-import { VStack } from "@chakra-ui/react"
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
+import SuggestedUser from "./SuggestedUser"
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
         <SuggestedHeader />
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+            <Text fontSize={12} fontWeight={'bold'} color={"gray.500"}>
+                Suggested for you
+            </Text> 
+            <Text fontSize={12} fontWeight={'bold'} _hover={{color:"gray.400"}} cursor={"pointer"}>
+                See All
+            </Text>
+        </Flex>
+        <SuggestedUser/>
+        <SuggestedUser/>
+        <SuggestedUser/>
+
+        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+            © 2024 Built By{" "}
+            <Link href='https://github.com/surujmiah6896' target='_blank' color='blue.500' fontSize={14}>
+                As a Programmer
+            </Link>
+        </Box>
     </VStack>
   )
 }
