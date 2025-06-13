deacb0370283798a4b0751fe77a54d181311b126
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-13 17:25:47 +0600

    design profile header

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import { Button, Flex, Text, VStack } from '@chakra-ui/react'
+import ProfileAvatar from '../ProfileAvatar'
 
 const ProfileHeader = () => {
   return (
-    <div>
-      Profile header
-    </div>
+    <>
+        <Flex 
+            gap={{base:4, sm:10}}
+            py={10}
+            direction={{base:"column", sm:"row"}}
+        >
+            <ProfileAvatar img="/img4.png" />
+
+            <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+                <Flex
+                    gap={4}
+                    direction={{base:"column", sm:"row"}}
+                    justifyContent={{base:"center", sm:"flex-start"}}
+                    alignItems={"center"}
+                    w={"full"}
+                >
+                    <Text fontSize={{base: "sm", md:"lg"}}>
+                        username
+                    </Text>
+                    <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+                        <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs", md:'sm'}}>
+                            Edit Profile
+                        </Button>
+
+                    </Flex>
+                </Flex>
+
+                <Flex alignItems={"center"} gap={{base:2, sm:4}}>
+                    <Text fontSize={{base:"xs", md:"sm"}}>
+                        <Text as="span" fontWeight={"bold"} mr={1}>4</Text>
+                        Posts
+                    </Text>
+                    <Text fontSize={{base:"xs", md:"sm"}}>
+                        <Text as="span" fontWeight={"bold"} mr={1}>234</Text>
+                        Followers
+                    </Text> 
+                    <Text fontSize={{base:"xs", md:"sm"}}>
+                        <Text as="span" fontWeight={"bold"} mr={1}>175</Text>
+                        Posts
+                    </Text>
+                </Flex>
+
+                <Flex alignItems={"center"} gap={4}>
+                    <Text fontSize={"sm"} fontWeight={"bold"}>
+                        title
+                    </Text>
+                </Flex>
+                <Text fontSize={"sm"}>
+                    user description dsfsd sdfsdfsdf sdfsd fsdfsdf  sdfsdfsdfsd sfdfsdfsdfsdfsdfsdfsdf
+                </Text>
+
+            </VStack>
+        </Flex>
+    </>
   )
 }
 
