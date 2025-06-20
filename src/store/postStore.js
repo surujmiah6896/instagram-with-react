c037771a8eb32eac2177602a73ecb49a2994fa78
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-20 20:22:24 +0600

    create hook for create post

---
+++ src/store/postStore.js
@@ -0,0 +1,20 @@
+import { create } from "zustand";
+const postStore = create((set) => ({
+    posts: [],
+    createPost: (post) => set((state) => ({posts: [post, ...state.posts] })),
+    deletePost: (id) => set((state) => ({posts: state.posts.filter((post) => post.id !== id)})),
+    setPosts: (posts) => set({posts}),
+    addComment: (postId, comment) => set((state) => ({
+        posts: state.posts.map((post) => {
+            if(post.id == postId){
+                return{
+                    ...post,
+                    comments: [...post.comments, comment],
+                }
+            }
+            return post;
+        }),
+    })),
+}));
+
+export default postStore
